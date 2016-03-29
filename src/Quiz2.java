a29495dc93100cafb795198da073dcf24829e2b5
pintm1551 <pintm1551@HRH-IC0027344.wrdsb.ca>
2016-03-29 08:24:17 -0400

    Quiz 2

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -4,7 +4,6 @@
  * and open the template in the editor.
  */
 
-
 import becker.robots.City;
 import becker.robots.Direction;
 import becker.robots.Robot;
@@ -22,9 +21,17 @@ public class Quiz2 {
         // Can use Wall1.txt, Wall2.txt, or Wall3.txt
         City kw = new City("Wall1.txt");
         Robot karel = new Robot(kw, 1, 1, Direction.WEST);
-        
-        //program solutions coded under here
-       
+
+        while (karel.getAvenue() == 1) { //while karl is in avenue 1
+            if (karel.frontIsClear()) {  //stops the loop if there is a break
+                break;
+            } else {                     //finds next wall
+                karel.turnLeft();       
+                karel.move();
+                karel.turnLeft();
+                karel.turnLeft();
+                karel.turnLeft();
+            }
+        }
     }
-    
 }
